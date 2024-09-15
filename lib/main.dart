5e7bb7b0d32c36e0cb71c29c6c8877e7ab4df98b
v0x <116308110+v0x@users.noreply.github.com>
2024-09-14 21:26:53 -0700

    refactor main.dart; create Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -25,26 +25,18 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: RootLayout(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class RootLayout extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _RootLayoutState createState() => _RootLayoutState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _controller = TextEditingController();
-  List<String> items = [];
+class _RootLayoutState extends State<RootLayout> {
   int currentPageIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchDataFromFirestore();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,35 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text('Submit Data to Firestore test again'),
         ),
         body: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    labelText: 'Test data',
-                  ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _sendDataToFirestore,
-                  child: Text('Submit'),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: items.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(items[index]),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
+          HomePage(pageIndex: currentPageIndex),
           TaskPage(),
           CalendarPage(),
           PomodoroPage(),
@@ -118,33 +82,5 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ));
-  }
-
-  void fetchDataFromFirestore() {
-    FirebaseFirestore.instance
-        .collection('data')
-        .orderBy('timestamp', descending: true)
-        .snapshots()
-        .listen((data) {
-      items.clear();
-      data.docs.forEach((doc) => items.add(doc['text']));
-      setState(() {});
-    });
-  }
-
-  void _sendDataToFirestore() {
-    String inputText = _controller.text;
-    if (inputText.isNotEmpty) {
-      FirebaseFirestore.instance
-          .collection('data')
-          .add({
-            'text': inputText,
-            'timestamp': FieldValue.serverTimestamp(),
-          })
-          .then((value) => print("Data Added"))
-          .catchError((error) => print("Failed to add data: $error"));
-
-      _controller.clear();
-    }
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  final pageIndex;
+  const HomePage({this.pageIndex, super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  List<String> items = [];
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFromFirestore();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Test data',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _sendDataToFirestore,
+              child: Text('Submit'),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, pageIndex) {
+                  return ListTile(
+                    title: Text(items[pageIndex]),
+                  );
+                },
+              ),
+            ),
+          ],
+        ));
+  }
+
+  void fetchDataFromFirestore() {
+    FirebaseFirestore.instance
+        .collection('data')
+        .orderBy('timestamp', descending: true)
+        .snapshots()
+        .listen((data) {
+      items.clear();
+      data.docs.forEach((doc) => items.add(doc['text']));
+      setState(() {});
+    });
+  }
+
+  void _sendDataToFirestore() {
+    String inputText = _controller.text;
+    if (inputText.isNotEmpty) {
+      FirebaseFirestore.instance
+          .collection('data')
+          .add({
+            'text': inputText,
+            'timestamp': FieldValue.serverTimestamp(),
+          })
+          .then((value) => print("Data Added"))
+          .catchError((error) => print("Failed to add data: $error"));
+
+      _controller.clear();
+    }
   }
 }
